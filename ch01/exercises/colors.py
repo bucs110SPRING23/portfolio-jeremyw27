fddfa931ea48310e524d526384273c57c5b77682
jeremyw27 <jeremyw0427@gmail.com>
2023-02-01 16:56:08 -0500

    colors

--- ch01/exercises/colors.py
+++ ch01/exercises/colors.py
@@ -1,5 +1,5 @@
 colors = ["red", "yellow", "blue", "green", "pink", "gray"]
-print("I have ", len(colors), " colors to choose from.")
+print("I have", len(colors), "colors to choose from.")
 print(colors[0])
 print(colors[1])
 print(colors[2])
