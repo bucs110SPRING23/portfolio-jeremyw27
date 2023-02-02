38d0d36609e67ff6f0748714e8706011b43ba5f7
jeremyw27 <jeremyw0427@gmail.com>
2023-02-02 01:46:57 -0500

    lab1

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,3 +1,4 @@
+import random
 #Part A
 weeks = 16
 classes = 5
@@ -7,7 +8,17 @@ print("Cost per week:", cost_per_week)
 
 classes_per_week = 5
 cost_per_class = cost_per_week/classes_per_week
-print("The cost per class is", cost_per_class, ".")
-print(classes_per_week, type(classes_per_week), cost_per_class, type(cost_per_class))
+print("Cost per class:", cost_per_class)
+print(
+    classes_per_week, type(classes_per_week),
+    cost_per_class, type(cost_per_class),
+    weeks, type(weeks),
+    classes, type(classes),
+    tuition, type(tuition),
+    cost_per_week, type(cost_per_week)
+)
 
 #Part B
+jeremy_list = ["hi", "3.3", "cs 110", "100", "2", "bye"]
+rand_result = random.choice(jeremy_list)
+print(rand_result)
