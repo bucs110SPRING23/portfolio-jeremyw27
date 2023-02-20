b3f02c299ed891964eadc489e2cdeffec4469732
jeremyw27 <jeremyw0427@gmail.com>
2023-02-20 17:27:11 -0500

    guess

--- ch03/exercises/guess.py
+++ ch03/exercises/guess.py
@@ -1,6 +1,7 @@
 import random
 rand = random.randrange(1,11)
 guess = int(input("Guess the number: "))
+print(guess)
 
 if guess == rand:
    print("correct! the number is ", rand)
@@ -8,10 +9,14 @@ else:
     for i in [2,1]:
       if guess > rand:
         print("Too high")
-        guess = input(i, " more guesses:")
+        guess = int(input(str(i) + " more guesses: "))
       elif guess < rand:
         print("Too low")
-        guess = input(i, " more guesses:")
-      else:
+        guess = int(input(str(i) + " more guesses: "))
+      elif guess == rand:
+        print("correct!")
+    if guess != rand:
+        print("incorrect!")
+    elif guess == rand:
         print("correct!")
     print("The number is ", rand)
