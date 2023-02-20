783223d16bcc295790507e42ea34aacdff6fc29d
jeremyw27 <jeremyw0427@gmail.com>
2023-02-20 17:33:24 -0500

    lab

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,4 +1,7 @@
 import pygame
 pygame.init()
 screen = pygame.display.set_mode()
-screen_size = display.get_window_size()
+screen_size = pygame.display.get_window_size()
+screen.fill("lightblue")
+pygame.display.flip()
+pygame.time.wait(1500)
