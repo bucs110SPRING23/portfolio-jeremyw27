a7a9c8700fe150ae5232790cd4ad8e326a48a64b
jeremyw27 <jeremyw0427@gmail.com>
2023-02-21 14:16:59 -0500

    lab 3 in prog

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,4 +1,6 @@
 import pygame
+import math
+import random
 pygame.init()
 
 # Part A
@@ -7,8 +9,18 @@ screen_size = pygame.display.get_window_size()
 screen.fill("lightblue")
 pygame.display.flip()
 pygame.draw.circle(screen, "black", [screen_size[0]/2, screen_size[1]/2], 300)
-pygame.draw.circle(screen, "red", [screen_size[0]/2, screen_size[1]/2], 298)
+pygame.draw.circle(screen, "white", [screen_size[0]/2, screen_size[1]/2], 298)
 pygame.display.flip()
-pygame.time.wait(2500)
 
 #Part B
+distance_from_center = math.hypot(x1-screen_size[0]/2, y1-screen_size[1]/2)
+is_in_circle = distance_from_center <= screen_size[0]/2
+for i in range(10):
+    if is_in_circle == True:
+        pygame.draw.circle(screen, "green", [random.randrange(screen_size[0]), random.randrange(screen_size[1])], 30)
+    else:
+        pygame.draw.circle(screen, "red", [random.randrange(screen_size[0]), random.randrange(screen_size[1])], 30)
+    pygame.display.flip()
+
+pygame.time.wait(2500)
+print(random.randrange(screen_size[0]))
