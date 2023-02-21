d0be332dc117a6fb4d9afd25eb056fa72b03cf02
jeremyw27 <jeremyw0427@gmail.com>
2023-02-20 19:29:04 -0500

    lab 3

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,7 +1,14 @@
 import pygame
 pygame.init()
-screen = pygame.display.set_mode()
+
+# Part A
+screen = pygame.display.set_mode([600, 600])
 screen_size = pygame.display.get_window_size()
 screen.fill("lightblue")
 pygame.display.flip()
-pygame.time.wait(1500)
+pygame.draw.circle(screen, "black", [screen_size[0]/2, screen_size[1]/2], 300)
+pygame.draw.circle(screen, "red", [screen_size[0]/2, screen_size[1]/2], 298)
+pygame.display.flip()
+pygame.time.wait(2500)
+
+#Part B
