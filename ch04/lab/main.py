6473a71db4df6531cfc34daf48484397b541f88c
jeremyw27 <jeremyw0427@gmail.com>
2023-03-06 21:42:55 -0500

    lab4

--- ch04/lab/main.py
+++ ch04/lab/main.py
@@ -3,13 +3,44 @@ import math
 import random
 pygame.init()
 
-# Part A
 screen = pygame.display.set_mode([600, 600])
-screen_size = pygame.display.get_window_size()
+width, height = pygame.display.get_window_size()
+
+done = False
+guess = []
+winner = []
+
+hitboxes = {
+    "blue": pygame.Rect(0, 0, width/2, height),
+    "forestgreen": pygame.Rect(0, 0, width/2, height),
+}
+
+hitboxes["forestgreen"].left = hitboxes["blue"].right
+
+pygame.draw.rect(screen, "blue", hitboxes["blue"])
+pygame.draw.rect(screen, "forestgreen", hitboxes["forestgreen"])
+pygame.display.flip()
+
+font = pygame.font.Font(None, 26)
+text = font.render("Guess who will win: blue (P1) or green (P2)?", True, "black")
+screen.blit(text, (width/4, height/2))
+pygame.display.flip()
+pygame.time.wait(150)
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if hitboxes["blue"].collidepoint(event.pos):
+                guess = 1
+                done = True
+            elif hitboxes["forestgreen"].collidepoint(event.pos):
+                guess = 2
+                done = True
+
 screen.fill("lightblue")
 pygame.display.flip()
-pygame.draw.circle(screen, "black", [screen_size[0]/2, screen_size[1]/2], 300)
-pygame.draw.circle(screen, "white", [screen_size[0]/2, screen_size[1]/2], 298)
+pygame.draw.circle(screen, "black", [width/2, height/2], 300)
+pygame.draw.circle(screen, "white", [width/2, height/2], 298)
 pygame.draw.line(screen, "black", [300, 0], [300, 600], 2)
 pygame.draw.line(screen, "black", [0, 300], [600, 300], 2)
 pygame.display.flip()
@@ -17,42 +48,60 @@ player1 = 0
 player2 = 0
 
 for i in range(10):
-    xcor = random.randrange(screen_size[0]+1)
-    ycor = random.randrange(screen_size[1]+1)
-    distance_from_center = math.hypot(xcor - screen_size[0]/2, ycor - screen_size[1]/2)
-    if distance_from_center <= screen_size[0]/2:
-        pygame.draw.circle(screen, "orange", [xcor, ycor], 12) #player 1 is orange
+    #Player 1's turn
+    x = random.randrange(width+1)
+    y = random.randrange(height+1)
+    x0 = width/2
+    y0 = height/2
+    distance_from_center = math.hypot(x - x0, y - y0)
+    is_in_circle = distance_from_center <= width/2
+    if is_in_circle == True:
+        pygame.draw.circle(screen, "blue", [x, y], 12) #player 1 is blue
         pygame.display.flip()
-        pygame.time.wait(400)
         player1 += 1
     else:
-        pygame.draw.circle(screen, "red", [xcor, ycor], 12) #red is outside the dartboard
+        pygame.draw.circle(screen, "red", [x, y], 12) #red is outside dartboard
         pygame.display.flip()
-        pygame.time.wait(400)
-    xcor = random.randrange(screen_size[0]+1)
-    ycor = random.randrange(screen_size[1]+1)
-    if distance_from_center <= screen_size[0]/2:
-        pygame.draw.circle(screen, "blue", [xcor, ycor], 12) #player 2 is blue
+    pygame.time.wait(300)
+    
+    #Player 2's turn
+    x = random.randrange(width+1)
+    y = random.randrange(height+1)
+    x0 = width/2
+    y0 = height/2
+    distance_from_center = math.hypot(x - x0, y - y0)
+    is_in_circle = distance_from_center <= width/2
+    if is_in_circle == True:
+        pygame.draw.circle(screen, "forestgreen", [x, y], 12) #player 2 is green
         pygame.display.flip()
-        pygame.time.wait(400)
         player2 += 1
     else:
-        pygame.draw.circle(screen, "red", [xcor, ycor], 12) #red is outside the dartboard
+        pygame.draw.circle(screen, "orange", [x, y], 12) #orange is outside dartboard
         pygame.display.flip()
-        pygame.time.wait(400)
+    pygame.time.wait(300)
 
 print(player1)
 print(player2)
-font = pygame.font.Font(None, 48)
 
+pygame.time.wait(500)
+font = pygame.font.Font(None, 48)
 if player1 > player2:
-    text = font.render("Player 1 wins the game! Score: " + str(player1) + "-" + str(player2), True, "black")
-    screen.blit(text, (screen_size[0]/2, screen_size[1]/2))
+    text = font.render("Player 1 wins! Score: " + str(player1) + "-" + str(player2), True, "black")
+    screen.blit(text, (width/4, height/2))
+    winner = 1
 elif player1 < player2:
-    text = font.render("Player 2 wins the game! Score: " + str(player1) + "-" + str(player2), True, "black")
-    screen.blit(text, (screen_size[0]/2, screen_size[1]/2))
+    text = font.render("Player 2 wins! Score: " + str(player1) + "-" + str(player2), True, "black")
+    screen.blit(text, (width/4, height/2))
+    winner = 2
 else:
     text = font.render("It's a tie! Score: " + str(player1) + "-" + str(player2), True, "black")
-    screen.blit(text, (screen_size[0]/2, screen_size[1]/2))
+    screen.blit(text, (width/4, height/2))
 
-pygame.time.wait(1200)
+if guess == winner:
+    text = font.render("Your guess was correct!", True, "black")
+    screen.blit(text, (width/4, 350))
+else:
+    text = font.render("Your guess was incorrect!", True, "black")
+    screen.blit(text, (width/4, 350))
+pygame.display.flip()
+pygame.time.wait(2500)
