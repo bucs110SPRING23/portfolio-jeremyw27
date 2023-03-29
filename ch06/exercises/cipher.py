5dd3d8d6b290396e6610917b81a5601d2201f949
jeremyw27 <jeremyw0427@gmail.com>
2023-03-29 03:56:47 -0400

    encrypted

--- ch06/exercises/cipher.py
+++ ch06/exercises/cipher.py
@@ -1,11 +1,16 @@
-def caesar_cipher(text, shift):
+def caesar_cipher(text, factor):
     result = ""
     for char in text:
         if char.isalpha():
             start = ord('A') if char.isupper() else ord('a')
+            shift = factor^(factor*7) * (factor*factor)^17
             new_pos = (ord(char) - start + shift) % 26
             char = chr(start + new_pos)
         result += char
     return result
 
-print(caesar_cipher("Hello", 1))
+message = caesar_cipher("The quick brown fox jumps over the lazy dog", 13)
+
+encrypted = open("encrypted.txt", "w")
+encrypted.write(message)
+encrypted.close()
